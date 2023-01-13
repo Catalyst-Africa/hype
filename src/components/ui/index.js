40580d6ab4f6f49823ae812f654f2b17bf80c5d9
Beyond-Logic <edemaero@gmail.com>
2023-01-13 15:32:03 +0100

    feat:verify-email

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,2 +1,3 @@
 export { default as InputGroup } from "./InputGroup";
 export { default as Logo } from "./Logo";
+export { default as OtpInput } from "./OtpInput";
---
+++ src/pages/auth/verifyEmail/index.jsx
@@ -0,0 +1,7 @@
+import OtpForm from "@/pages/auth/verifyEmail/molecules/OtpForm";
+
+const index = () => {
+  return <OtpForm />;
+};
+
+export default index;
