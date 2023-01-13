8efd8c67ef76d5d889578ec2b0bd3b8a2a229b74
browynlouis <browynlouis2@gmail.com>
2023-01-13 02:09:42 -0800

    feat: Added IconContext for react-icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { IconContext } from "react-icons";
 
 import App from "./App";
 import { ScrollToTop } from "@/components/functional";
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <GlobalStyles />
       <Router>
         <ScrollToTop />
-        <App />
+        <IconContext.Provider value={{ size: "24px" }}>
+          <App />
+        </IconContext.Provider>
       </Router>
     </CustomThemeProvider>
   </React.StrictMode>,
