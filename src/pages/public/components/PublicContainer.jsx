8511b2d0e7664a6104f7a988ff20229303ee1d5b
Beyond-Logic <edemaero@gmail.com>
2023-02-11 14:32:17 +0100

    feat:  updates, styles, layout, home, hypeview

--- src/pages/public/components/PublicContainer.jsx
+++ src/pages/public/components/PublicContainer.jsx
@@ -1,29 +1,8 @@
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
-import lovebg from "../../../assets/hypesbg/lovebg.svg";
-import lovebg1 from "../../../assets/hypesbg/lovebg1.svg";
 
 const PublicContainer = ({ children, style }) => {
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return (
-    <Container
-      style={style}
-      // backgroundImage: `url(${width > 1023 ? lovebg : lovebg1})`,
-      // width: "100%",
-    >
-      {children}
-    </Container>
-  );
+  return <Container style={style}>{children}</Container>;
 };
 
 PublicContainer.propTypes = {
@@ -38,12 +17,9 @@ const Container = styled.div`
   flex-direction: column;
   gap: 30px;
   width: 100%;
-  height: 100vh;
   margin: 0 auto;
+
   /* padding: 22px 51px; */
-  background-repeat: no-repeat;
-  background-position: right top;
-  background-size: contain;
 
   ${({ theme }) => theme.breakpoints.down("md")} {
     background-size: contain;
