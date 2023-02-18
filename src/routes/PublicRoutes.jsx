c9fc0a0eab33379cac29823ad0646d3b10e61d3d
Beyond-Logic <edemaero@gmail.com>
2023-02-18 18:45:25 +0100

    Feat: Updates

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,12 +1,13 @@
 import { Route } from "react-router-dom";
 import { PublicLayout } from "@/layouts";
-import { Home, HypeMessage, NotFound, Terms } from "../pages/public";
+import { Home, HypeMessage, NotFound, Privacy, Terms } from "../pages/public";
 
 const publicRoutes = (
   <>
     <Route path="/" element={<PublicLayout />}>
       <Route path="" element={<Home />} />
       <Route path="/terms" element={<Terms />} />
+      <Route path="/privacy" element={<Privacy />} />
       <Route path="/hype/message/:id" element={<HypeMessage />} />
       <Route path="*" element={<NotFound />} />
     </Route>
